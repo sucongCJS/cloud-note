0256b433f6f71bb54a5650804cd3d4a2c12798bb
Jason <sucongjs@gmail.com>
2018-10-02 18:15:47 +0800

    当在输入的时候，清除错误提示

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// 检测输入是否空白
+function fnCheckBlank(){
+    var list = document.getElementsByTagName("input")
+    for(var i = 0; i < list.length; i++){
+        if(list[i].value == "" || list[i].value == null){
+            document.getElementById("error_box").innerHTML = "请填写完整"
+            return false
+        }
+    }
+}
+
+function fnClearError(){
+    document.getElementById("error_box").innerHTML = ""
+}
